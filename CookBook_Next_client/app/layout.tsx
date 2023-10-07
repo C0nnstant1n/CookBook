9054946dc09bc506cb17982e2487015a37b12948
C0nstant1n <c0nnstant1n.cs@gmail.com>
2023-10-07 20:48:02 +0700

    learn next.js react

---
+++ CookBook_Next_client/app/layout.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/Header";
+import "./globals.css";
+import type { Metadata } from "next";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Cook Book",
+  description: "Cook book on next.js and react",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ CookBook_Next_client/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className='main '>
+      <h1>
+        Добро пожаловать на сайт<hr></hr>кулинарных рецептов
+      </h1>
+    </main>
+  );
+}
